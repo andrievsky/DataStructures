6473d11e6cf9c733452e6d6a7c5dc624e1275eb1
Nick Andrievsky <toflasher@gmail.com>
2017-09-19 10:08:18 +0200

    Added the tasts description

--- Assignments/Week1/TreeHeight.cs
+++ Assignments/Week1/TreeHeight.cs
@@ -5,6 +5,21 @@ using Assignments.Common;
 
 namespace Assignments.Week1
 {
+    /// <summary>
+    /// Task. You are given a description of a rooted tree. Your task is to compute and output its height. Recall
+    /// that the height of a (rooted) tree is the maximum depth of a node, or the maximum distance from a
+    /// leaf to the root. You are given an arbitrary tree, not necessarily a binary tree.
+    /// Input Format. The first line contains the number of nodes 𝑛. The second line contains 𝑛 integer numbers
+    /// from −1 to 𝑛 − 1 — parents of nodes. If the 𝑖-th one of them (0 ≤ 𝑖 ≤ 𝑛 − 1) is −1, node 𝑖 is the root,
+    /// otherwise it’s 0-based index of the parent of 𝑖-th node. It is guaranteed that there is exactly one root.
+    /// It is guaranteed that the input represents a tree.
+    /// 
+    /// Constraints. 
+    /// 1 ≤ 𝑛 ≤ 10^5.
+    ///
+    /// Output Format. 
+    /// Output the height of the tree.
+    /// </summary>
     public class TreeHeight : IAssignment
     {
         public string Execute(string[] args)
