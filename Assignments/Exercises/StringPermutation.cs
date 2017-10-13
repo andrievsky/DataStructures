95fbb2c46839b10c6a0cd4929fe562f72f37caf0
Nick Andrievsky <nandrievsky@megazebra.com>
2017-10-13 18:39:10 +0200

    Added the first part of permutation

---
+++ Assignments/Exercises/StringPermutation.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignments.Exercises
+{
+    public class StringPermutation
+    {
+        public void Permutation(string source, string prefix)
+        {
+            if (source.Length == 0)
+            {
+                //Console.WriteLine(prefix);
+                return;
+            }
+            for (int i = 0; i < source.Length; i++)
+            {
+                string rem = source.Substring(0, i) + source.Substring(i + 1);
+                Permutation(rem, prefix + source[i]);
+            }
+        }
+    }
+}
--- Assignments/Main.cs
+++ Assignments/Main.cs
@@ -1,10 +1,36 @@
-﻿namespace Assignments
+﻿using System;
+using System.Diagnostics;
+using Assignments.Exercises;
+
+namespace Assignments
 {
     /// <summary>
     /// The app entry point
     /// </summary>
     public class Assignments
     {
-        private static void Main(string[] args){}
+        private const int RepeatCount = 100;
+
+        private static void Main(string[] args)
+        {
+            CheckPerforamance(() =>
+            {
+                var excersise = new StringPermutation();
+                excersise.Permutation("abcdefghm", "");
+            }, "Permutation");
+        }
+
+        private static void CheckPerforamance(Action action, string name)
+        {
+            action();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (var i = 0; i < RepeatCount; i++)
+            {
+                action();
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Task {name} complete in {stopWatch.ElapsedMilliseconds / RepeatCount} milliseconds");
+        }
     }
 }
